9eda955b1845fa0f592aeb9f5fcc024dfb3cdb24
yama-choki <lovepingpong0@gmail.com>
2022-11-17 12:05:37 +0900

    ログアウト後の遷移先をログインページに変更

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -31,10 +31,10 @@
 
                         <x-slot name="content">
                             <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
+                            <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
-                                <x-dropdown-link :href="route('logout')"
+                                <x-dropdown-link :href="route('login')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
                                     {{ __('ログアウト') }}
